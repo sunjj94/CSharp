5aa1b5f7f75999c329c72d568c7ad5703a87f49a
sunjj <jingjing.sun@ganx.com.cn>
2017-11-24 08:49:34 +0800

    存款计算器实现

---
+++ 00CSCalculate/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace _00CSCalculate
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBoxInterest = new System.Windows.Forms.TextBox();
+            this.textBoxYears = new System.Windows.Forms.TextBox();
+            this.textBoxCorpus = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(19, 143);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "计算";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(121, 143);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "退出";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // textBoxInterest
+            // 
+            this.textBoxInterest.Location = new System.Drawing.Point(96, 98);
+            this.textBoxInterest.Name = "textBoxInterest";
+            this.textBoxInterest.Size = new System.Drawing.Size(100, 21);
+            this.textBoxInterest.TabIndex = 2;
+            // 
+            // textBoxYears
+            // 
+            this.textBoxYears.Location = new System.Drawing.Point(96, 67);
+            this.textBoxYears.Name = "textBoxYears";
+            this.textBoxYears.Size = new System.Drawing.Size(100, 21);
+            this.textBoxYears.TabIndex = 3;
+            // 
+            // textBoxCorpus
+            // 
+            this.textBoxCorpus.Location = new System.Drawing.Point(96, 34);
+            this.textBoxCorpus.Name = "textBoxCorpus";
+            this.textBoxCorpus.Size = new System.Drawing.Size(100, 21);
+            this.textBoxCorpus.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "最初本金";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 12);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "存期（年）";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(17, 104);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 12);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "年利率（%）";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Items.AddRange(new object[] {
+            "存期\t\t本金\t\t利息\t\t总金额"});
+            this.listBox1.Location = new System.Drawing.Point(221, 34);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(341, 124);
+            this.listBox1.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(574, 195);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxCorpus);
+            this.Controls.Add(this.textBoxYears);
+            this.Controls.Add(this.textBoxInterest);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "存款计算器";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBoxInterest;
+        private System.Windows.Forms.TextBox textBoxYears;
+        private System.Windows.Forms.TextBox textBoxCorpus;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ 00CSCalculate/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _00CSCalculate
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double dCorpus;
+            int iYears;
+            double dInterest;
+            string str;
+            double dIncome;
+            double dInterestIncom;
+
+            try
+            {
+                dCorpus = double.Parse(textBoxCorpus.Text);
+                iYears = Int32.Parse(textBoxYears.Text);
+                dInterest = double.Parse(textBoxInterest.Text) / 100;
+            }
+            catch
+            {
+                MessageBox.Show("输入非数字");
+                return;
+            }
+
+            dIncome = dCorpus * Math.Pow((1 + dInterest), iYears);
+            dInterestIncom = dIncome - dCorpus;
+
+            str = string.Format("{0, 3:d} {1, 15:c} {2, 16:c} {3, 18:c}", iYears, dCorpus, dInterestIncom, dIncome);
+            listBox1.Items.Add(str);
+        }
+    }
+}
