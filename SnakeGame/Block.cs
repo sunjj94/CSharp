ca59ff96d9f26cd070c121b74ecf8387f58c82b1
sunjj <jingjing.sun@ganx.com.cn>
2017-11-24 14:47:00 +0800

    添加Block类

---
+++ SnakeGame/Block.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace SnakeGame
+{
+    public class Block
+    {
+        //编号
+        private int number;
+        //（左上角）位置
+        private Point origin;
+
+        //编号属性
+        public int Number
+        {
+            get { return number; }
+            set { number = value; }
+        }
+
+        //位置属性
+        public Point Origin
+        {
+            get { return origin; }
+            set { origin = value; }
+        }
+
+        //绘制块
+        public void Display(Graphics g)
+        {
+            Pen pen = new Pen(Color.Red);
+            g.DrawRectangle(pen, origin.X, origin.Y, 5, 5);
+        }
+
+        //消除块
+        public void UnDisplay(Graphics g)
+        {
+            Pen pen = new Pen(Color.Silver);
+            g.DrawRectangle(pen, origin.X, origin.Y, 5, 5);
+        }
+    }
+}
